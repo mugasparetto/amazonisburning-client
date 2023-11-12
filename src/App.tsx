2e23a66ad1ec953878637580d4f5e60524f2e1ff
mugasparetto <mu.gasparetto@hotmail.com>
2023-11-12 14:38:12 +0000

    listening to socket event

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { socket } from './socket';
 
 function App() {
-  return <h1>Hello world</h1>;
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    function onConnect() {
+      console.log('connected');
+    }
+
+    function onDisconnect() {
+      console.log('disconnected');
+    }
+
+    function onMessage(value: any) {
+      setCounter(value);
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+    socket.on('message', onMessage);
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+      socket.off('message', onMessage);
+    };
+  }, []);
+
+  return <h1>Hello world {counter}</h1>;
 }
 
 export default App;
