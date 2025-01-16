a2c1a2e5cd5b063c7baf9ae22ced51445e6ade72
mugasparetto <mu.gasparetto@hotmail.com>
2025-01-16 07:41:58 +0000

    fieldio setup

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -66,7 +66,7 @@ const Main: React.FC = () => {
       <Content open={portalOpen}>
         <h1>{wildfiresCount}</h1>
         <Description>
-          Fires burning in the Amazon rainforest since I sent my CV to Field.IO
+          Fires burning in the Amazon rainforest since I sent my CV to FIELD.IO
         </Description>
         <TimeContainer>
           <TimeElapsed />
